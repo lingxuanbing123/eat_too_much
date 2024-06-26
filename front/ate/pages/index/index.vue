d11b3d65e5267a3da26bf1c6a880537527105b05
haoxichangzai <18942168925@163.com>
2024-06-26 16:23:14 +0800

    首页v3

--- front/ate/pages/index/index.vue
+++ front/ate/pages/index/index.vue
@@ -24,8 +24,6 @@
 				<view class="searchButton">
 					搜索
 				</view>
-				
-				
 			</view>
 
 			<!--美食外卖  -->
@@ -36,13 +34,11 @@
 				</view>
 			</view>
 			
-			
 			<view class="indexList">
 				<view class="takeOutCard">
 					<!-- 左边大图图片 -->
 					<view class="cardImage">
-						
-						<image class="cardImagePic" src="../../static/indexCard1.jpg"></image>
+						<image class="cardImagePic" src="../../static/indexCard3.jpg"></image>
 					</view>
 					
 					<!-- 右边描述 -->
@@ -68,14 +64,13 @@
 							近30日373人复购
 						</view>
 						
-						
 						<!-- 下拉框按钮 -->
-						<view class="hideButton">
-							<image class="hideButtonImg" src="/hideButton1.png"></image>
+						<view class="hideButton" @click="showHide">
+							<image class="hideButtonImg" :src="hideIm"></image>
 						</view>
 						
 						<!-- 未点击隐藏按钮之前显示的内容 -->
-						<!-- <view class="hide">
+						<view v-if="isHide" class="hide">
 							<view class="hide1">
 								<image class="hide1Img" src="../../static/hide1.png"></image>
 								9.5元无门槛
@@ -86,29 +81,52 @@
 							<view class="hide3">
 								35减3
 							</view>
-						</view> -->
+						</view>
 						<!-- 点击隐藏按钮之后显示的内容 -->
-						<view class="unHide">
+						<view v-else class="unHide">
 							<view class="activity">
 								活动
+								<view class="act1">
+									25减2
+								</view>
+								<view class="act2">
+									35减3
+								</view>
+								<view class="act3">
+									55减5
+								</view>
+								<view class="act4">
+									首次光顾减1
+								</view>
 							</view>
 							<view class="redPocket">
 								红包
+								<view class="rP1">
+									红包满28减8
+								</view>
+								<view class="rP2">
+									9.5元无门槛
+								</view>
+								<view class="rP3">
+									3.5元店铺红包
+								</view>
 							</view>
 							<view class="service">
 								服务
+								<view class="sv1">
+									先享后付
+								</view>
+								<view class="sv2">
+									食无优
+								</view>
+								<view class="sv3">
+									极速退
+								</view>
 							</view>
 						</view>
-						
-					
-						
 					</view>
-					
 				</view>
 			</view>
-		</view>
-		<view class="tabbar">
-			tabbar
 		</view>
 		
 	</view>
@@ -118,69 +136,69 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				isHide: true,
+				hideIm:"../../static/hideButton1.png"
+				
 			}
 		},
 		onLoad() {
 
 		},
 		methods: {
-
+			showHide() {
+				this.isHide = !this.isHide;
+				this.hideIm = this.isHide ? "../../static/hideButton1.png":"../../static/hideButton2.png"; 
+			}
 		}
 	}
 </script>
 
 <style>
-.container{
+.container {
 	position: relative;
 	background-color: whitesmoke;
 	min-height: 100vh;
 }
 
-.container .indexTitle{
+.container .indexTitle {
 	width: 100%;
 	height: 70rpx;
 	background-color: whitesmoke;
 	display: flex;
 	flex-direction: row;
 	padding: 20rpx;
-	
 }
 
-.indexTitle .buttonBox{
+.indexTitle .buttonBox {
 	display: flex;
 	flex-direction: row;
 	font-size: 40rpx;
 	height: 50rpx;
-	/* background-color: bisque; */
 }
 
-.buttonBox .titleButton1{
+.buttonBox .titleButton1 {
 	padding: 5rpx;
 	border-right: 4rpx solid #D8D8D8;
-	/* #D8D8D8是常点未选中的颜色 */
 }
 
-.buttonBox .titleButton2{
+.buttonBox .titleButton2 {
 	padding: 5rpx;
 }
 
-.indexTitle .location{
+.indexTitle .location {
 	display: flex;
 	flex-direction: row;
-	margin-left:250rpx;
+	margin-left: 250rpx;
 	padding: 5rpx;
 	font-size: 31rpx;
 }
 
-
-
-.prefer .indexSearch{
+.prefer .indexSearch {
 	width: 720rpx;
 	height: 50rpx;
 	background-color: #FEFFFA;
 	border-radius: 50rpx;
-	border: 4rpx solid #28ADE6 ;
+	border: 4rpx solid #28ADE6;
 	padding: 5rpx;
 	display: flex;
 	flex-direction: row;
@@ -189,13 +207,12 @@
 	margin-left: 10rpx;
 }
 
-.indexSearch .searchText{
+.indexSearch .searchText {
 	padding: 5rpx;
 	width: 600rpx;
-	/* background-color: red; */
 }
 
-.indexSearch .searchButton{
+.indexSearch .searchButton {
 	width: 120rpx;
 	height: 45rpx;
 	padding: 5rpx;
@@ -207,83 +224,75 @@
 	border-radius: 50rpx;
 	display: flex;
 	justify-content: center;
-	
 }
 
-
-.prefer .indexSquare{
+.prefer .indexSquare {
 	width: 100%;
 	height: 400rpx;
-	background-color:lightblue;
+	background-color: lightblue;
 }
 
-.prefer .indexList{
+.prefer .indexList {
 	width: 100%;
-	height: 600rpx;
-	/* background-color:bisque; */
 }
 
-.indexList .takeOutCard{
+.indexList .takeOutCard {
 	width: 700rpx;
-	height: 300rpx;
+	height: auto;
 	display: flex;
 	flex-direction: row;
-	justify-content:space-between;
-	align-items: center;
-	margin: 15rpx 0 0 20rpx;
+	justify-content: start;
+	margin: 15rpx 0 0 15rpx;
 	border-radius: 20rpx;
-	background-color: #FFFFFF;
+	padding: 15rpx;
+	background-color: white;
 }
 
-.takeOutCard .cardImage{
+.takeOutCard .cardImage {
 	width: 180rpx;
-	height: 300rpx;
+	margin-top: 20rpx;
 }
 
-.cardImage .cardImagePic{
+.cardImage .cardImagePic {
 	width: 180rpx;
 	height: 180rpx;
 	border-radius: 10rpx;
 	display: flex;
 	align-items: center;
-	margin: 20rpx 0 0 20rpx ;
 }
 
-.takeOutCard .cardDescribe{
-	width: 490rpx;
-	height: 300rpx;
-	background-color: #FFFFFF;
+.takeOutCard .cardDescribe {
+	flex-grow: 1;
 	border-top-right-radius: 20rpx;
 	border-bottom-right-radius: 20rpx;
 	display: flex;
 	flex-direction: column;
+	padding: 5rpx;
 }
 
-.cardDescribe .cardName{
-
+.cardDescribe .cardName {
 	font-size: 30rpx;
 	margin: 10rpx 0 0 0;
 }
 
-.cardDescribe .cardevaluate{
+.cardDescribe .cardevaluate {
 	display: flex;
 	flex-direction: row;
 	justify-content: space-between;
-	/* background-color: lightblue; */
 	font-size: 23rpx;
 	color: gray;
 }
 
-.cardevaluate .cardeValuateGrade{
+.cardevaluate .cardeValuateGrade {
 	color: orange;
 }
 
-.cardDescribe .deliveryFee{
+.cardDescribe .deliveryFee {
 	font-size: 20rpx;
 	color: gray;
 }
 
-.cardDescribe .reBuy{
+.cardDescribe .reBuy {
 	width: 200rpx;
 	display: flex;
 	flex-direction: row;
@@ -294,79 +303,102 @@
 	border-radius: 20rpx;
 	margin: 10rpx 0 0 0;
 }
-.reBuy .reBuyLeft{
+
+.reBuy .reBuyLeft {
 	background-color: #FFD8C1;
 	border-top-left-radius: 20rpx;
 	border-bottom-left-radius: 20rpx;
-	
 }
 
-.reBuy .reBuyImage{
+.reBuy .reBuyImage {
 	width: 25rpx;
 	height: 25rpx;
 }
 
-.cardDescribe .hideButton{
-	
+.cardDescribe .hideButton {
 }
 
-.hideButton .hideButtonImg{
+.hideButton .hideButtonImg {
 	width: 30rpx;
 	height: 30rpx;
 	margin: 0rpx 0 0 400rpx;
 }
 
-.cardDescribe .hide{
+.cardDescribe .hide {
 	width: 300rpx;
 	display: flex;
 	flex-direction: row;
 	justify-content: space-between;
 	font-size: 20rpx;
 	margin: -30rpx 0 0 0;
-	
 }
 
-.hide .hide1{
+.hide .hide1 {
 	padding: 3rpx;
-	border: 2rpx solid #D1944F ;
+	border: 2rpx solid #D1944F;
 	color: #D1944F;
 	border-radius: 5rpx;
 }
 
-.hide1 .hide1Img{
+.hide1 .hide1Img {
 	width: 20rpx;
 	height: 20rpx;
 }
 
-.hide .hide2{
+.hide .hide2,.hide3 {
 	color: #C97A81;
-	border: 2rpx solid  #C97A10;
+	border: 2rpx solid #C97A10;
 	border-radius: 5rpx;
 }
 
-.hide .hide3{
-	border: 2rpx solid  #C97A10;
+.unHide .activity {
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	font-size: 20rpx;
+	color: gray;
+}
+
+.activity .act1, .act2, .act3, .act4 {
+	color: red;
+	border: 2rpx solid #C97A81;
 	border-radius: 5rpx;
-	color: #C97A81;
 }
 
-.cardDescribe .activity{
-	background-color: palevioletred;
+.unHide .redPocket {
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	font-size: 20rpx;
+	color: gray;
+	margin-top: 10rpx;
 }
 
-
-
-.cardDescribe .redPocket{
-	background-color: brown;
+.redPocket .rP1, .rP2, .rP3 {
+	color: red;
+	border: 2rpx solid #C97A81;
+	border-radius: 5rpx;
 }
 
-.cardDescribe .service{
-	background-color: lawngreen;
+.unHide .service {
+	display: flex;
+	flex-direction: row;
+	justify-content: start;
+	font-size: 20rpx;
+	color: gray;
+	margin-top: 10rpx;
 }
 
-.container .tabbar{
+.unHide .sv1, .sv2, .sv3 {
+	color: black;
+	border: 2rpx solid black;
+	border-radius: 5rpx;
+	margin-left: 10rpx;
+}
+
+.container .tabbar {
 	width: 100%;
 	height: 400rpx;
-	background-color:brown;
+	background-color: brown;
 }
 </style>
