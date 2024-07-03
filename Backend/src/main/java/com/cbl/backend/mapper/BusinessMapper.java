c10cca45619ea66fb90bc425ac3990c672290a6c
lingxuanbing123 <llt2lxy@163.com>
2024-07-03 21:40:14 +0800

    商家完成

--- Backend/src/main/java/com/cbl/backend/mapper/BusinessMapper.java
+++ Backend/src/main/java/com/cbl/backend/mapper/BusinessMapper.java
@@ -1,6 +1,7 @@
 package com.cbl.backend.mapper;
 
 import com.cbl.backend.entity.Business;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -8,4 +9,8 @@ import java.util.List;
 public interface BusinessMapper {
     @Select("SELECT * FROM `business_info`")
     List<Business> getAll();
+
+    @Select("SELECT * FROM business_info WHERE businessId = #{businessId}")
+    Business getBusinessById(@Param("businessId") int businessId);
+
 }
